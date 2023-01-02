b41ef0d8d7e73546d2c62618f2432de7d9be191c
shivamgupta83 <shivam8359083814@gmail.com>
2023-01-02 12:55:21 +0530

    Done

--- src/models/vaccineModel.js
+++ src/models/vaccineModel.js
@@ -32,22 +32,24 @@ const vaccineSlots = new mongoose.Schema({
         type: Number,
         required: true
     },
-    timeSlots: [{
-        "10:00AM-10:30AM": { type: String, enum: ["Available", "Unavailable"] },
-        "10:30AM-11:00AM": { type: String, enum: ["Available", "Unavailable"] },
-        "11:00AM-11:30AM": { type: String, enum: ["Available", "Unavailable"] },
-        "11:30AM-12:00PM": { type: String, enum: ["Available", "Unavailable"] },
-        "01:00PM-01:30PM": { type: String, enum: ["Available", "Unavailable"] },
-        "01:30PM-02:00PM": { type: String, enum: ["Available", "Unavailable"] },
-        "02:00PM-02:30PM": { type: String, enum: ["Available", "Unavailable"] },
-        "02:30PM-03:00PM": { type: String, enum: ["Available", "Unavailable"] },
-        "03:00PM-03:30PM": { type: String, enum: ["Available", "Unavailable"] },
-        "03:30PM-04:00PM": { type: String, enum: ["Available", "Unavailable"] },
-        "04:00PM-04:30PM": { type: String, enum: ["Available", "Unavailable"] },
-        "04:30PM-05:00PM": { type: String, enum: ["Available", "Unavailable"] },
-    _id:0
-    }],
+    timeSlots: {
+        type:String,
+    enum:[
+        "10:00AM-10:30AM",
+        "10:30AM-11:00AM",
+        "11:00AM-11:30AM",
+        "11:30AM-12:00PM",
+        "01:00PM-01:30PM",
+        "01:30PM-02:00PM",
+        "02:00PM-02:30PM",
+        "02:30PM-03:00PM",
+        "03:00PM-03:30PM",
+        "03:30PM-04:00PM",
+        "04:00PM-04:30PM",
+        "04:30PM-05:00PM",
+        ]
+}
     
-})
+},{timestamps:true})
 
 module.exports = mongoose.model('vaccineSlots', vaccineSlots)
